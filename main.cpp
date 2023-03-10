d0f9221d206a406de39b742c00d4146d9ba73bf8
Yusuf Kara <94810547+yusufkara95@users.noreply.github.com>
2023-03-10 02:40:42 +0100

    Execute code not on heap anymore and remove the pointer etc.

--- main.cpp
+++ main.cpp
@@ -1,64 +1,26 @@
 #include <iostream>
 
 #include "battle.h"
-#include "mymonster.h"
-#include "enemymonster.h"
 #include "player.h"
-#include "item.h"
-#include "market.h"
 
 using namespace std;
 
 int main()
 {
 
-    MyMonster* wassiris = new MyMonster("Wassiris", 30, "Wasser", 8, 16, 4, "blau");
-    MyMonster* flanzoros = new MyMonster("Flanzoros", 34, "Pflanze", 8, 16, 4, "gruen");
-    MyMonster* feuerpol = new MyMonster("Feuerpol", 40, "Feuer", 8, 16, 4, "rot");
+    Monster flanzoros ("Flanzoros", 34, "Pflanze", 7, 16, 4);
+    Monster ofenos ("Ofenos", 30, "Dunkelheit", 5, 12, 2);
 
-    EnemyMonster* ofenos = new EnemyMonster("Ofenos", 20, "Dunkelheit", 8, 12, 2, "rot");
+    MyTeam playerTeam;
+    Player player("Yuga", "m", 100, playerTeam);
 
 
-    MyTeam* playerTeam = new MyTeam();
-    //Inventory* playerInventory = new Inventory();
-
-    Market* market = new Market("MonMarket");
-    Item* potion = new Item("Trank", 20, 50);
-    Item* monball = new Item("Monball", 0, 25);
-
-    Player* player = new Player("Yuga", "m", 100, *playerTeam);
+    std::cout << "-------------------------------------------" << std::endl;
+    player.printPlayer();
+    std::cout << "-------------------------------------------" << std::endl;
 
     Battle* battleStation = new Battle();
-
-    market->addItem(potion);
-    market->addItem(monball);
-
-    //market->printMarket();
-
-    playerTeam->addMonster(*wassiris);
-
-
-    std::cout << "-------------------------------------------" << std::endl;
-    player->printPlayer();
-    std::cout << "-------------------------------------------" << std::endl;
-    playerTeam->printMyTeam();
-    std::cout << "-------------------------------------------" << std::endl;
-
-
-    battleStation->startBattle(*flanzoros, *ofenos);
-
-
-
-    delete wassiris;
-    delete feuerpol;
-    delete flanzoros;
-
-    delete market;
-    delete potion;
-    delete monball;
-
-    delete player;
-    delete playerTeam;
+    battleStation->startBattle(flanzoros, ofenos);
 
 }
 
